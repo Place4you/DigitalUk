0592deff1a149229275ec1cd64f9bd60b8004a53
Place4you <66410030+Place4you@users.noreply.github.com>
2023-09-06 15:06:30 +0500

    first commit

---
+++ assets/script.js
@@ -0,0 +1,89 @@
+gsap.registerPlugin(ScrollTrigger);
+// page 1 ---------------------------------------
+    gsap.from("#page1 #a img",{
+        y:150,
+        duration:3, 
+        ease:"power3",
+        stagger:.4
+    });
+
+    gsap.from("#page1 #b img",{
+        x:150,
+        duration:3, 
+        ease:"power3",
+        stagger:.2,
+    });
+    gsap.from("#page1 #c img",{
+        y:-150,
+        duration:3, 
+        ease:"power3",
+        stagger:.2,
+    });
+    gsap.from("#page1 h1",{
+        x:-150,
+        duration:3, 
+        ease:"power3",
+        stagger:.2,
+    });
+
+// page 2 ---------------------------------------
+
+gsap.from("#page2 h1, #page2 h3 ",{
+    opacity:0,
+    y:150,
+    duration:1, 
+    ease:"power3",
+    stagger:.2,
+    scrollTrigger:{
+        trigger:'#page2',
+        scroller: 'body',
+        // markers: true,
+        start: "top 85%",
+        end: "bottom 34%",
+        scrub:true
+    }
+});
+
+
+
+    tl = gsap.timeline();
+    
+        tl.from("#page3 #pgright",{
+                opacity:0,
+                y:150,
+                duration:3, 
+                ease:"power3",
+                stagger:.2,
+                scrollTrigger:{
+                    trigger:"#page3",
+                    scroller:"body",
+                    start:"top 70%",
+                    end:"bottom 50%",
+                    // markers:true,
+                    scrub:true
+                     }
+        });
+
+        tl.from("#page3 #pgleft img",{
+            opacity:0,
+            x:-150,
+            duration:4, 
+            ease:"power4",
+            // stagger:.4,
+            scrollTrigger:{
+                trigger:'#page3',
+                scroller:"body",
+                // markers:true,
+                start:"top 60%",
+                end:"bottom 30%",
+                scrub:true
+                 }
+    });
+
+    toggle =document.querySelector("#menu-toggle:checked")
+    logo = document.querySelector("#logo img")
+    if( toggle == true){
+        logo.style.display = "none"
+    }
+        
+
